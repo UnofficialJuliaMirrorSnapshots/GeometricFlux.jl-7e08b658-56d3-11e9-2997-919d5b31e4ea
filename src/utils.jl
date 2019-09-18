711d9e5250ff031a06e5ede3d19f312f3ab9fb55
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-18 11:57:03 +0000

    Snapshot of branch develop taken on 2019-09-18T07:57:01.98-04:00 from "https://github.com/UnofficialJuliaMirror/GeometricFlux.jl-7e08b658-56d3-11e9-2997-919d5b31e4ea"

--- src/utils.jl
+++ src/utils.jl
@@ -3,10 +3,19 @@ function gather(input::Array{T,N}, index::Array{<:Integer,N}, dims::Integer;
     @assert dims <= N "Specified dimensions must lower or equal to the rank of input matrix."
 
     @inbounds for x = CartesianIndices(out)
-        ind = Tuple(x)
-        tup = collect(ind)
-        tup[dims] = index[ind...]
-        out[ind...] = input[tup...]
+        tup = collect(Tuple(x))
+        tup[dims] = index[x]
+        out[x] = input[tup...]
+    end
+    return out
+end
+
+
+function gather(input::Matrix{T}, index::Array{Int}) where T
+    out = Array{T}(undef, size(input,1), size(index)...)
+    for ind = CartesianIndices(index)
+        k = index[ind]
+        out[:, ind] = input[:, k]
     end
     return out
 end
